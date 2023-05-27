91bc8aa1b7dd0c6d180a2c758b105e805e858b4a
Simon Marquis <contact@simon-marquis.fr>
2023-05-27 18:08:42 +0200

    🪂 Code drop!

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
-# svg2avd
+# ⚙️ svg2avd
+
+> Kotlin script to convert SVG files to **A**ndroid **V**ector **D**rawables.
+
+### Usage
+
+```bash
+./svg2avd.main.kts --help
+```
+
+```
+Usage: svg2avd.main.kts [OPTIONS]
+
+  ⚙️ svg2avd: Convert SVG files to Android Vector Drawables (AVD).
+
+Options:
+  -i, --input PATH     SVG assets input directory
+  -o, --output PATH    AVD assets output directory
+  -p, --precision INT  Set number of digits in the fractional part (svgo)
+  -q, --quiet          Print errors only
+  -h, --help           Show this message and exit
+```
+
+### Example
+
+```bash
+./svg2avd.main.kts --input=icons 
+```
+
+```
+⚙️ Converting icons/alarm.svg to icons/alarm.xml
+⚙️ Converting icons/delete.svg to icons/delete.xml
+⚙️ Converting icons/lightbulb.svg to icons/lightbulb.xml
+
+✅ /home/svg2avd/icons
+```
+
+### SVG compression with [svgo](https://github.com/svg/svgo)
+
+```bash
+./svg2avd.main.kts --input=icons --precision=2
+```
+
+```
+Processing directory '/home/svg2avd/icons':
+
+
+alarm.svg:
+Done in 17 ms!
+0.386 KiB - 17% = 0.32 KiB
+
+delete.svg:
+Done in 3 ms!
+0.21 KiB - 9.8% = 0.189 KiB
+
+lightbulb.svg:
+Done in 4 ms!
+0.288 KiB - 11.9% = 0.254 KiB
+
+⚙️ Converting /tmp/alarm.svg to icons/alarm.xml
+⚙️ Converting /tmp/delete.svg to icons/delete.xml
+⚙️ Converting /tmp/lightbulb.svg to icons/lightbulb.xml
+
+✅ /home/svg2avd/svg2avd/icons
+```
+
+### Credits
+
+- [com.android.tools:sdk-common](https://maven.google.com/web/index.html?q=sdk-common#com.android.tools:sdk-common)
+  > sdk-common library used by other Android tools libraries.
+- [ajalt/clikt](https://github.com/ajalt/clikt)
+  > Multiplatform command line interface parsing for Kotlin
+- [svg/svgo](https://github.com/svg/svgo)
+  > Node.js tool for optimizing SVG files
+- [jakearchibald/svgomg](https://jakearchibald.github.io/svgomg/)
+  > Web GUI for SVGO
+- [google/material-design-icons](https://github.com/google/material-design-icons)
+  > Material Design icons by Google
---
+++ svg2avd.main.kts
@@ -0,0 +1,79 @@
+#!/usr/bin/env kotlin
+
+@file:Repository("https://repo1.maven.org/maven2/")
+@file:Repository("https://maven.google.com")
+
+@file:DependsOn("org.jetbrains.kotlin:kotlin-stdlib:1.9.0-Beta")
+@file:DependsOn("com.android.tools:sdk-common:31.0.2")
+@file:DependsOn("com.github.ajalt.clikt:clikt-jvm:3.5.2")
+
+import com.android.ide.common.vectordrawable.Svg2Vector
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.check
+import com.github.ajalt.clikt.parameters.options.defaultLazy
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.prompt
+import com.github.ajalt.clikt.parameters.types.int
+import com.github.ajalt.clikt.parameters.types.path
+import java.lang.System.lineSeparator
+import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.io.path.absolutePathString
+import kotlin.io.path.createTempDirectory
+import kotlin.io.path.extension
+import kotlin.io.path.isRegularFile
+import kotlin.io.path.nameWithoutExtension
+import kotlin.io.path.outputStream
+
+class SVG2AVD : CliktCommand(
+    name = "svg2avd.main.kts",
+    help = "⚙️ svg2avd: Convert SVG files to Android Vector Drawables (AVD).",
+) {
+
+    private val input: Path by option("-i", "--input", help = "SVG assets input directory")
+        .path(mustExist = true, canBeFile = false, mustBeReadable = true)
+        .prompt("SVG assets input directory")
+
+    private val output: Path by option("-o", "--output", help = "AVD assets output directory")
+        .path(mustExist = true, canBeFile = false, mustBeWritable = true)
+        .defaultLazy { input }
+
+    private val precision: Int? by option("-p", "--precision", help = "Set number of digits in the fractional part (svgo)")
+        .int().check("must be a positive integer") { it >= 0 }
+
+    private val quiet by option("-q", "--quiet", help = "Print errors only")
+        .flag(default = false)
+
+    override fun run() = input
+        .ifNotNull(precision) { compress(precision = it) }
+        .files(extension = "svg")
+        .parallel().forEach { it.convert(output) }
+        .also { if (!quiet) echo(lineSeparator() + "✅ " + output.absolutePathString()) }
+
+    private fun Path.compress(precision: Int) = createTempDirectory().also {
+        val svgo = exec(
+            "svgo",
+            "--folder=${absolutePathString()}",
+            "--output=${it.absolutePathString()}",
+            "--precision=$precision",
+            "--multipass",
+        )
+        if (!quiet) echo(svgo + lineSeparator())
+    }
+
+    private fun Path.convert(output: Path) = output
+        .resolve("${nameWithoutExtension.toSnakeCase()}.xml")
+        .also { if (!quiet) echo("⚙️ Converting $this to $it") }
+        .outputStream().use { Svg2Vector.parseSvgToXml(this, it) }
+
+    private fun Path.files() = Files.walk(this).filter { it: Path -> it.isRegularFile() }
+    private fun Path.files(extension: String) = files().filter { it.extension == extension }
+    private fun exec(vararg cmd: String) = Runtime.getRuntime().exec((arrayOf("cmd", "/c").takeIf { isWindows() } ?: emptyArray()) + cmd).text()
+    private fun Process.text() = apply { waitFor() }.inputStream.bufferedReader().use { it.readText().trim() }
+    private fun String.toSnakeCase(): String = "(?<=.)[A-Z]".toRegex().replace(this, "_$0").lowercase()
+    private fun <I : Any, O : Any?> I.ifNotNull(it: O?, block: I.(O) -> I): I = if (it != null) block(it) else this
+    private fun isWindows() = System.getProperty("os.name").orEmpty().startsWith("windows", ignoreCase = true)
+}
+
+SVG2AVD().main(args)
